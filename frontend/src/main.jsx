caa9cfb32d3cc594997a933977fe02eec8ebfd56
mazenfteha <mazn34903@gmail.com>
2023-09-24 20:08:02 -0700

    react router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowerRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowerRouter>
     <App />
-  </React.StrictMode>,
+  </BrowerRouter>,
 )
